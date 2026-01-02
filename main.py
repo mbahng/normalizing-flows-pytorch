2e788d382abf90c24c03a5d05631dd037274d3c6
Muchang Bahng <bahngmc@gmail.com>
2026-01-02 04:48:54 -0500

    config

--- main.py
+++ main.py
@@ -9,32 +9,41 @@ torch.manual_seed(42)
 torch.cuda.manual_seed(42)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-ds = flowers102()
+ds = mnist()
+# ds = flowers102()
 dl = torch.utils.data.DataLoader(ds, batch_size=16, shuffle=True) 
 
-# model = RealNVP(
+# model = NICE(
 #   idim=ds[0][0].size(), 
-#   n_scales=2, # should always be 2 for cifar10
-#   neural_net_layers=8,
-#   hdim=torch.Size([64]), 
+#   n_coupling_layers=6, 
+#   neural_net_layers=8, 
+#   hdim=torch.Size([256]), 
 #   device=device
 # )
 
-model = Glow(
-  idim=ds[0][0].size(), 
-  n_levels=7, # should always be 2 for cifar10 
-  flow_depth=3, 
-  neural_net_layers=4,
+model = RealNVP(
+  idim=torch.Size([1, 28, 28]),
+  n_scales=2, # should always be 2 for cifar10
+  neural_net_layers=8,
   hdim=torch.Size([64]), 
   device=device
 )
+
+# model = Glow(
+#   idim=ds[0][0].size(), 
+#   n_levels=7, # should always be 2 for cifar10 
+#   flow_depth=3, 
+#   neural_net_layers=4,
+#   hdim=torch.Size([64]), 
+#   device=device
+# )
 
 optimizer = torch.optim.Adam(params=model.parameters(), lr=1e-3)
 
 best_loss = float('inf')
 
 # viz.plot_distribution(model, ds) 
-viz.plot_samples(model, savefig="fig/0.png")
+# viz.plot_samples(model, savefig="fig/0.png")
 
 for epoch in range(50): 
   loss = tnt.train(model, dl, optimizer)
@@ -42,8 +51,8 @@ for epoch in range(50):
   if loss < best_loss: 
     torch.save(model.state_dict(), f"saved/glow-state-dict.pt")
 
-  # viz.plot_distribution(model, ds)
-  viz.plot_samples(model, savefig=f"fig/{epoch+1}.png")
+  viz.plot_distribution(model, ds)
+  # viz.plot_samples(model, savefig=f"fig/{epoch+1}.png")
 
 # viz.plot_distribution(model, ds, savefig="fig/readme/moons.png")
 # viz.plot_samples(model, savefig=f"fig/{epoch+1}.png")
